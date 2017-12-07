b4d709a725409ac1abdbce21991af5514b6ffd65
annickvdp <annick.van.den.put@cvovolt.be>
2017-12-07 14:00:31 +0100

    Taal: opdrachten

---
+++ main/lang/dutch2/trad4all.inc.php
@@ -0,0 +1,17 @@
+<?php
+$AddAssignmentPage="opdrachten";
+$HWork="opdrachten";
+$Student_publication="Opdrachten\n                        ";
+$StudentPublications="Opdrachten\n                        ";
+$ToolStudentPublication="Opdrachten\n                        ";
+$Work="Opdrachten";
+$Works="Opdrachten";
+$NewStudentPublicationCreated="De nieuwe opdracht is aangemaakt\n                        ";
+$WorkEmailAlertActivate="opdracht";
+$WorkEmailAlertDeactivate="opdracht";
+$TitleManipulateStudentPublication="Opdracht wijzigen             ";
+$StudentPublicationSelectionForGradebookTitle="Opdrachten voor in het puntenboek            ";
+$PublicationStartDate="                        ";
+$PublicationEndDate="                        ";
+$MoveCurrentStudentPublication="Wijzig de opdracht van de huidige student                        ";
+$EditCurrentStudentPublication="Wijzig de opdracht van de huidige student                        ";
